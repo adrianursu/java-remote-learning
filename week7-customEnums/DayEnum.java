58733d6040b3c75c68063493ce47fb1206636965
Adrian Ursu <uursu.adrian@gmail.com>
2021-03-05 00:21:28 +0200

    custom-enums updated

--- week7-customEnums/DayEnum.java
+++ week7-customEnums/DayEnum.java
@@ -9,5 +9,3 @@ public enum DayEnum {
     FRIDAY,
     SATURDAY;
 }
-
-
--- week7-customEnums/Simulator.java
+++ week7-customEnums/Simulator.java
@@ -6,6 +6,9 @@ public class Simulator {
 
     public static void main(String[] args) {
         Date date = new Date();
-        System.out.println(DayEnum.values()[0] + ", " + MonthEnum.values()[2] + ", " + (date.getYear() + 1900));
+        Date date1 = new Date(1997, 1, 4);
+
+        System.out.println(DayEnum.values()[date1.getDay()] + ", " + MonthEnum.values()[date1.getMonth()] + ", " + (date1.getYear()));
+        System.out.println(DayEnum.values()[date.getDay()] + ", " + MonthEnum.values()[date.getMonth()] + ", " + (date.getYear() + 1900));
     }
 }
