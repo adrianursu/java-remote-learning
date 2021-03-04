1746c4c07266016bc18e3dd97820d0d93b422898
Adrian Ursu <uursu.adrian@gmail.com>
2021-03-04 23:39:12 +0200

    Updated storyTeller

--- week7-storyTeller/Main.java
+++ week7-storyTeller/Main.java
@@ -3,6 +3,6 @@ package main.java.iquest.com.storyTeller;
 public class Main {
     public static void main(String[] args) {
         StoryMaker storyMaker = new StoryMaker();
-        System.out.println(storyMaker.CreateStory());
+        System.out.println(storyMaker.createStory());
     }
 }
--- week7-storyTeller/StoryMaker.java
+++ week7-storyTeller/StoryMaker.java
@@ -8,14 +8,20 @@ public class StoryMaker {
     private static final String[] verbs = {"drove", "jumped", "ran", "walked", "skipped"};
     private static final String[] prepositions = {"the", "a", "to", "in", "of"};
 
-    private StringBuilder stringBuilder = new StringBuilder();
+    private StringBuilder stringBuilder;
 
-    public String CreateStory() {
+    public StoryMaker() {
+        this.stringBuilder = new StringBuilder();
+    }
+
+    public String createStory() {
       for(int i = 0; i < 20; i++) {
           int index1 = new Random().nextInt(5);
           int index2 = new Random().nextInt(5);
           int index3 = new Random().nextInt(5);
           int index4 = new Random().nextInt(5);
+          int index5 = new Random().nextInt(5);
+          int index6 = new Random().nextInt(5);
 
             stringBuilder.append(Character.toUpperCase(articles[index1].charAt(0)))
                     .append(articles[index1].substring(1))
@@ -25,6 +31,10 @@ public class StoryMaker {
                     .append(verbs[index3])
                     .append(" ")
                     .append(prepositions[index4])
+                    .append(" ")
+                    .append(articles[index5])
+                    .append(" ")
+                    .append(nouns[index6])
                     .append(".");
         }
         return stringBuilder.toString();
