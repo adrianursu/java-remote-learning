bd710bec5a3aeaa89be7012948f782b6afd5089d
Adrian Ursu <uursu.adrian@gmail.com>
2021-03-09 13:28:30 +0200

    renamed folder names + refactored the folder structure

---
+++ week7/pig-latin/Main.java
@@ -0,0 +1,8 @@
+package main.java.iquest.com.pigLatin;
+
+public class Main {
+    public static void main(String[] args) {
+        PigLatin pigLatin = new PigLatin();
+        System.out.println(pigLatin.translate("computer monkey the aah aah"));
+    }
+}
