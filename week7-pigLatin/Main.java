c734f39ea480562ecf17c23a96d243f79defbacd
Adrian Ursu <uursu.adrian@gmail.com>
2021-03-09 13:06:51 +0200

    pigLatin problem update

--- week7-pigLatin/Main.java
+++ week7-pigLatin/Main.java
@@ -3,6 +3,6 @@ package main.java.iquest.com.pigLatin;
 public class Main {
     public static void main(String[] args) {
         PigLatin pigLatin = new PigLatin();
-        System.out.println(pigLatin.convertToPigLatin("computer monkey the aah aah"));
+        System.out.println(pigLatin.translate("computer monkey the aah aah"));
     }
 }
--- week7-pigLatin/PigLatin.java
+++ week7-pigLatin/PigLatin.java
@@ -2,41 +2,30 @@ package main.java.iquest.com.pigLatin;
 
 public class PigLatin {
 
-    private StringBuilder stringBuilder;
+    private StringBuilder converter;
 
     public PigLatin() {
-        this.stringBuilder = new StringBuilder();
+        this.converter = new StringBuilder();
     }
 
-    public String convertToPigLatin(String string) {
+    public String translate(String string) {
 
         String[] words = string.split("\\s+");
 
         for (int i = 0; i < words.length; i++) {
             if (isVowel(words[i].toLowerCase().charAt(0))) {
-                stringBuilder.append(words[i])
-                        .append("ay");
+                converter.append(words[i]);
             } else {
-                stringBuilder.append(words[i].substring(1))
-                        .append(words[i].substring(0, 1))
-                        .append("ay");
+                converter.append(words[i].substring(1))
+                        .append(words[i].substring(0, 1));
             }
-            stringBuilder.append(" ");
+            converter.append("ay");
+            converter.append(" ");
         }
-        return stringBuilder.toString().trim();
+        return converter.toString().trim();
     }
 
     private static boolean isVowel(char c) {
-        if (c == 'a')
-            return true;
-        if (c == 'e')
-            return true;
-        if (c == 'i')
-            return true;
-        if (c == 'o')
-            return true;
-        if (c == 'u')
-            return true;
-        return false;
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
